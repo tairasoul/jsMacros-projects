97d4cedc96ed234ea3a2ded30dee245a69f7bfbc
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-04-28 02:57:35 +0200

    Update mappedInventory.d.ts

--- lib/mappedInventory.d.ts
+++ lib/mappedInventory.d.ts
@@ -1,4 +1,14 @@
 export = class MappedInventory {
-    slots: Array<{slot: number, itemHelper: $ItemStackHelper}>;
-    addItem(item: {slot: number, itemHelper: $ItemStackHelper}): void
+    slots:{
+        armor: Array<{slot: number, itemHelper: $ItemStackHelper}>,
+        crafting: Array<{slot: number, itemHelper: $ItemStackHelper}>,
+        inventory: Array<{slot: number, itemHelper: $ItemStackHelper}>,
+        hotbar: Array<{slot: number, itemHelper: $ItemStackHelper}>,
+        offhand: null | $ItemStackHelper
+    };
+    addInventory(item: {slot: number, itemHelper: $ItemStackHelper}): void
+    addArmor(item: {slot: number, itemHelper: $ItemStackHelper}): void
+    addCrafting(item: {slot: number, itemHelper: $ItemStackHelper}): void
+    setOffhand(item: {slot: number, itemHelper: $ItemStackHelper}): void
+    addHotbar(item: {slot: number, itemHelper: $ItemStackHelper}): void
 }
