8d279425fd8b4d3b856b9bab0a6709cc3aaa1a43
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-04-30 12:38:13 +0200

    Update mappedInventory.d.ts

--- lib/mappedInventory.d.ts
+++ lib/mappedInventory.d.ts
@@ -1,16 +1,16 @@
 interface ArmorSlot {
     slot: number;
     itemHelper: $ItemStackHelper;
-    defense: number;
 }
 
 export = class MappedInventory {
     slots:{
-        armor: {helmet: ArmorSlot, chestplate: ArmorSlot, leggings: ArmorSlot, boots: ArmorSlot}
+        armor: {helmet: ArmorSlot, chestplate: ArmorSlot, leggings: ArmorSlot, boots: ArmorSlot},
         crafting: Array<{slot: number, itemHelper: $ItemStackHelper}>,
         inventory: Array<{slot: number, itemHelper: $ItemStackHelper}>,
         hotbar: Array<{slot: number, itemHelper: $ItemStackHelper}>,
         offhand: null | $ItemStackHelper
+        all: Array<{slot: number, itemHelper: $ItemStackHelper}>
     };
     addInventory(item: {slot: number, itemHelper: $ItemStackHelper}): void
     addArmor(item: {slot: number, itemHelper: $ItemStackHelper}): void
