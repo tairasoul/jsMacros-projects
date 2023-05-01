1664d753ac9a39c75bb79ba3d075348f362cae2a
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-05-01 20:01:47 +0200

    Update WSChat.js

--- WSChat/WSChat.js
+++ WSChat/WSChat.js
@@ -4,10 +4,6 @@ if (event && event.message) {
         event.message = null;
         const ws = GlobalVars.getObject("WSConnection").ws
         ws.sendText(`${Player.getPlayer().getName().getString()}&&${message.slice(1)}`)
-        Chat.log(Chat.createTextBuilder().append("[").append('WS').withColor(2).append("] ")
-            .append(Player.getPlayer().getName().getString()).withColor(10)
-            .append(": ").withColor(0x7).append(message.slice(1)).withColor(9)
-            .build());
     }
 }
 
