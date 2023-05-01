06585a972c418eae958854660c9d7da58acb0e9d
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-05-01 19:40:16 +0200

    Create Backend.js

---
+++ WSChat/Backend.js
@@ -0,0 +1,29 @@
+const ws = require('ws');
+const {exec} = require('child_process')
+
+const wss = new ws.Server({
+    port: 8080
+})
+
+const clients = [];
+
+wss.on('connection', (socket) => {
+    clients.push(socket)
+    console.log('new client connected')
+    socket.on('message', (message) => {
+        const msg = message.toString();
+        for (const client of clients) {
+            const sent = JSON.stringify(msg.split('&&'));
+            if (sent.length > 1) client.send(sent)
+        }
+        console.log(msg)
+    })
+    socket.on('close', () => {
+        console.log('client disconnected')
+        for (const client of clients) {
+            if (client == socket) {
+                clients[client] = null
+            }
+        }
+    })
+})
