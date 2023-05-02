7711cab87e74acc672021d9c73a8bac22b49e9e0
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-05-02 15:43:21 +0200

    fix disconnecting randomly and not reconnecting

--- WSChat/setWS.js
+++ WSChat/setWS.js
@@ -1,4 +1,4 @@
-const WSURL = "YOUR-BACKEND-URL-HERE"
+const WSUrl = "YOUR-BACKEND-URL"
 
 let ws = Request.createWS(WSUrl).connect();
 
@@ -7,13 +7,30 @@ ws.onConnect = JavaWrapper.methodToJava(() => {
             .build());
 })
 
-ws.onDisconnect = JavaWrapper.methodToJava(() => {
-    Time.sleep(1000)
-    ws = Request.createWS(WSUrl).connect()
-    GlobalVars.putObject('WSConnection', {
-        ws: ws
-    })
-})
+let method; method = (websocket, /** @type {Websocket$Disconnected} */disconnected) => {
+    if (!disconnected.isServer) {
+        Time.sleep(500);
+        ws = Request.createWS(WSUrl).connect();
+        ws.onConnect = JavaWrapper.methodToJava(() => {
+            Chat.log(Chat.createTextBuilder().append("[").append('WS').withColor(2).append("] ").append('Connected to ' + WSUrl).withColor(9)
+                    .build());
+        })
+        ws.onTextMessage = JavaWrapper.methodToJava((ws, msg) => {
+            const message = JSON.parse(msg)
+            Chat.log(Chat.createTextBuilder().append("[").append('WS').withColor(2).append("] <")
+                    .append(message[0]).withColor(10)
+                    .append("> ").withColor(0x7).append(message[1]).withColor(9)
+                    .build());
+        })
+        GlobalVars.putObject('WSConnection', {
+            ws: ws
+        })
+        ws.onDisconnect = JavaWrapper.methodToJava(method)
+    }
+}
+
+ws.onDisconnect = JavaWrapper.methodToJava(method)
+
 ws.onTextMessage = JavaWrapper.methodToJava((ws, msg) => {
     const message = JSON.parse(msg)
     Chat.log(Chat.createTextBuilder().append("[").append('WS').withColor(2).append("] <")
