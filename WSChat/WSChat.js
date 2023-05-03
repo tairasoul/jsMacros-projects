6f3f7fe911a6f12a5b39723e435481c739fa062a
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-05-03 16:24:16 +0200

    update to buffer if not connected

--- WSChat/WSChat.js
+++ WSChat/WSChat.js
@@ -2,7 +2,13 @@ if (event && event.message) {
     if (event.message.startsWith('.')) {
         const message = event.message;
         event.message = null;
-        const ws = GlobalVars.getObject("WSConnection").ws
+        const wso = GlobalVars.getObject("WSConnection");
+        const ws = wso.ws;
+        if (!ws) {
+            while (!ws) {
+                Time.sleep(10)
+            }
+        }
         ws.sendText(`${Player.getPlayer().getName().getString()}&&${message.slice(1)}`)
     }
 }
