6c168464e180f0b3c26ebffe8e1922e45d36fe52
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-05-03 16:08:05 +0200

    buffer messages if client disconnects improperly

--- WSChat/Backend.js
+++ WSChat/Backend.js
@@ -1,28 +1,69 @@
-const ws = require('ws');
+function createBackend(port) {
+    const ws = require('ws');
 
-const wss = new ws.Server({
-    port: 8080
-})
-
-const clients = [];
-
-wss.on('connection', (socket) => {
-    clients.push(socket)
-    console.log('new client connected')
-    socket.on('message', (message) => {
-        const msg = message.toString();
-        for (const client of clients) {
-            const sent = JSON.stringify(msg.split('&&'));
-            if (sent.length > 1) client.send(sent)
-        }
-        console.log(msg)
+    const wss = new ws.Server({
+        port: port
     })
-    socket.on('close', () => {
-        console.log('client disconnected')
-        for (const client of clients) {
-            if (client == socket) {
-                clients[client] = null
+
+    const clients = [];
+    let bufferedMessages = [];
+    let bufferMessages = false;
+    let disconnectedClients = {
+
+    };
+
+    wss.on('connection', (socket) => {
+        let socketLastMessage;
+        let clientName;
+        clients.push(socket)
+        console.log(`[${port}] new client connected`)
+        socket.on('message', (message) => {
+            socketLastMessage = message.toString()
+            const msg = message.toString();
+            const split = msg.split('&&');
+            if (split && split[1] == "connected") {
+                for (const prop in disconnectedClients) {
+                    if (disconnectedClients[prop].name == split[0]) {
+                        bufferMessages = false;
+                        for (const bufferedMessage of bufferedMessages) {
+                            socket.send(bufferedMessage)
+                        }
+                    }
+                }
+                clientName = split[0];
             }
-        }
+            else {
+                for (const client of clients) {
+                    const sent = JSON.stringify(msg.split('&&'));
+                    if (sent.length > 1) client.send(sent)
+                    if (bufferMessages) {
+                        bufferedMessages.push(sent)
+                    }
+                }
+                
+            }
+            console.log(`[${port}] ${msg}`)
+            
+        })
+        socket.on('close', () => {
+            console.log(`[${port}] client disconnected`)
+            if (clientName) {
+                if (socketLastMessage != "disconnect") {
+                    for (const client of clients) {
+                        if (client == socket) {
+                            clients[client] = null;
+                            socket.removeAllListeners('message');
+                            socket.removeAllListeners('close');
+                        }
+                    }
+                }
+                else {
+                    bufferMessages = true
+                    disconnectedClients[clientName] = {
+                        name: clientName
+                    }
+                }
+            }
+        })
     })
-})
+}
