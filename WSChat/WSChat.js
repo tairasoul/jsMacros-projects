e420cf6421d5bd8a0a1eca2b58f9dea2b8ba3eb8
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-05-30 14:54:29 +0200

    fix: it should now properly wait for ws to exist

--- WSChat/WSChat.js
+++ WSChat/WSChat.js
@@ -2,11 +2,11 @@ if (event && event.message) {
     if (event.message.startsWith('.')) {
         const message = event.message;
         event.message = null;
-        const wso = GlobalVars.getObject("WSConnection");
-        const ws = wso.ws;
+        let ws = GlobalVars.getObject("WSConnection").ws;
         if (!ws) {
             while (!ws) {
-                Time.sleep(10)
+                Time.sleep(10);
+                ws = GlobalVars.getObject("WSConnection").ws;
             }
         }
         ws.sendText(`${Player.getPlayer().getName().getString()}&&${message.slice(1)}`)
